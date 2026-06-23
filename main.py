f756459b461c7c20dd61eb1628d0b53fb3190e91
ZHHHCCC <z212199lroulin@outlook.com>
2026-06-23 20:00:49 +0800

    完成任务三

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pre_process import data
 from time_analyse import time_analysis
+from route_analyse import analyze_route_stops,visualize
 
 a=data()
 a.test()
@@ -8,3 +9,7 @@ df=a.delete()
 b=time_analysis()
 b.ea_to_tally()
 b.visualize_ea()
+
+
+print(analyze_route_stops(df).head(10))
+visualize(analyze_route_stops(df))
